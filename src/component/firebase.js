52e8acac58913369ec097c0cedb9924d1b577240
Saleamlak21 <fentaneshmesfin@gmail.com>
2023-10-03 15:10:47 -0400

    modify the firebase componenet

--- src/component/firebase.js
+++ src/component/firebase.js
@@ -1,13 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD4Qeccidu7m7GAW8HaLtC4dIKYuL1uzk0",
   authDomain: "fifth-base-398400.firebaseapp.com",
@@ -15,11 +9,12 @@ const firebaseConfig = {
   storageBucket: "fifth-base-398400.appspot.com",
   messagingSenderId: "504480769425",
   appId: "1:504480769425:web:23b0a53235e3d4ca796630",
-  measurementId: "G-HEXBS95DXM"
+  measurementId: "G-HEXBS95DXM",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth= getAuth(app)
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = app.firestore();
 
-export {auth}
+export { auth, db };

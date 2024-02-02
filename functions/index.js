6982db490f14368aa25cd3566af415b0c8d76856
Saleamlak21 <fentaneshmesfin@gmail.com>
2024-02-02 13:35:33 -0500

    modifications

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,35 @@
-require("dotenv").config();
+require("dotenv").config(); // Load environment variables from a .env file
+
+// Import required libraries and modules
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(process.env.stripe,);
 
-const app = express();
+const app = express(); // Create an Express application
+app.use(cors({origin: true})); // Automatically allow cross-origin requests
+app.use(express.json()); // Automatically parse request body as JSON
 
-app.use(cors({origin: true}));
-app.use(express.json());
-
-app.get("/", (request, response) => response.status(200).send("hello world"));
-
+// Define a route to handle payment creation
 app.post("/payments/create", async (request, response) => {
   try {
+    // Get the total from the request body
     const total = request.query.total;
 
+    // Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
       amount: total,
       currency: "usd",
     });
-
+// Send PaymentIntent details to client
     response.status(201).send({
       clientSecret: paymentIntent.client_secret,
     });
   } catch (error) {
+    // Handle errors
     console.error("Error creating payment intent:", error);
     response.status(500).send(error);
   }
 });
-
+// Export the Express app as an HTTP Cloud Function
 exports.api = functions.https.onRequest(app);
--- src/component/context/Reducer.js
+++ src/component/context/Reducer.js
@@ -1,21 +1,27 @@
+// create an initial state for the basket and user
 export const initialState = {
   basket: [],
   user: null,
 };
 
+// create a reducer to manage the state of the basket and user
 const reducer = (state, action) => {
-  // console.log(action)
+  // check the action type
   switch (action.type) {
+
+    // if the action type is ADD_TO_BASKET
     case "ADD_TO_BASKET":
       return {
         ...state,
         basket: [...state.basket, action.item],
       };
+      
+    // if the action type is Remove_From_Array
     case "Remove_From_Array":
       const index = state.basket.findIndex(
         (basketItem) => basketItem.id == action.id
       );
-
+      // create a new basket
       const newBasket = [...state.basket];
 
       if (index > -1) {
@@ -28,21 +34,24 @@ const reducer = (state, action) => {
         basket: newBasket,
       };
 
+    // if the action type is SET-USER
     case "SET-USER":
       return {
         ...state,
         user: action.user,
       };
 
+    // if the action type is CLEAN_BASKET
     case "CLEAN_BASKET":
       return {
         ...state,
         basket: [],
       };
 
+   // if the action type is not found
     default:
-      return state;
+      return state; 
   }
 };
-
+// export the reducer
 export default reducer;
